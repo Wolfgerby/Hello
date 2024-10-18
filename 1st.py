9b5ec2b8ffdec02199ec74dfd868d85ef6025cf1
wolfgerby <wolfgerby@gmail.com>
2024-10-18 00:00:24 -0400

    new

---
+++ 1st.py
@@ -0,0 +1,11 @@
+print("Hello World")
+name = input("name: ")
+print("hello, " + name)
+print(f"hello, {name}")
+n = int(input("number: "))
+if n > 0:
+	print("number is positive " + name)
+elif n < 0:
+	print("number is negative " +name)
+else:
+	print("nunmber is zero " + name)
