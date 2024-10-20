6709a2b07db31ee39ca4419f59448d91e3402551
Wolfgerby <wolfgerby@gmail.com>
2024-10-19 20:09:56 -0400

    Budget1.py

--- Budget.py
+++ Budget.py
@@ -1,30 +1,38 @@
-#budget tools
-budget={"income":[],
-	"expenses":[]
-	}
-#function
-#add
+budget = {
+    "income": [],
+    "expenses": []
+}
+
+# function to add income
 def add_income(budget):
-	amount=float(input("Enter income amount plz:"))
-	budget["income"].append(amount)	
-	Print(f"Added income:{amount}")
+    amount = float(input("Enter income amount plz: "))
+    budget["income"].append(amount)
+    print(f"Added income: {amount}")
+
+# function to add expense
 def add_expense(budget):
-	amount=float(input("Enter expense/spending amount plz"))
-	budget["expenses"].append(amount)
-	print(f"added expenses:{amount}")
-#total
+    amount = float(input("Enter expense/spending amount plz: "))
+    budget["expenses"].append(amount)
+    print(f"Added expenses: {amount}")
+
+# function to calculate total balance
 def calculate_balance(budget):
-	total_income=sum(budget["income"])
-	total_expenses=sum(budget["expenses"])
-	balance=total_income-total_expenses
-	return total_income,total_expenses,balance
-#loop
-while True:action=input("Do you want to add income, add expenses, or view balance?(type 'exit' to quit):").lower()
-if action == 'add income':add_income(budget)
-elif action == 'add expense':add_expense(budget)
-elif action == 'view balance':toltal_income,total_expense,balance=calculate_balance(budget)
-print(f"Total Income:{total_income}), Total expenses:{total_expenses},Balance:{balance}")
-elif action == 'exit':
-	break
-else:
-	print("invalid try again plz")
+    total_income = sum(budget["income"])
+    total_expenses = sum(budget["expenses"])
+    balance = total_income - total_expenses
+    return total_income, total_expenses, balance
+
+# loop
+while True:
+    action = input("Do you want to add income, add expenses, or view balance? (type 'exit' to quit): ").lower()
+    if action == 'add income':
+        add_income(budget)
+    elif action == 'add expense':
+        add_expense(budget)
+    elif action == 'view balance':
+        total_income, total_expenses, balance = calculate_balance(budget)
+        print(f"Total Income: {total_income}, Total Expenses: {total_expenses}, Balance: {balance}")
+    elif action == 'exit':
+        break
+    else:
+        print("Invalid option, try again plz.")
